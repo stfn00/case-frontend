de24dc93353324512f3d54f00c38bec582973050
Stefano Iachetta <stefano.iachetta@hotmail.it>
2023-12-16 00:51:46 +0100

    feat(styled): add new props forwarding

--- src/lib/registry.js
+++ src/lib/registry.js
@@ -7,7 +7,7 @@ import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 export default function StyledComponentsRegistry({ children }) {
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
 
-  const hiddenStyledProps = ['colorScheme']
+  const hiddenStyledProps = ['colorScheme', 'variant', 'iconEnd']
 
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement()

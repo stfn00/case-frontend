41d154ae3d83ff01c52b6d0e3cbbf6784fb82687
Stefano Iachetta <stefano.iachetta@hotmail.it>
2023-12-18 18:15:17 +0100

    feat: add Header sticky animation

--- src/components/layouts/Header/index.jsx
+++ src/components/layouts/Header/index.jsx
@@ -1,5 +1,9 @@
 'use client'
 
+import { useState } from 'react'
+
+import useEventListener from '@/hooks/useEventListener'
+
 import { Container, Visible } from '@/components/atoms/GridSystem'
 import Typography from '@/components/atoms/Typography'
 import Button from '@/components/atoms/Button'
@@ -7,17 +11,42 @@ import Button from '@/components/atoms/Button'
 import * as S from './styles'
 
 const Header = () => {
+  const [isSticky, setIsSticky] = useState(false)
+
+  let timeout
+  let lastScrollTop = 0
+
+  const handleScroll = () => {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+
+    timeout = setTimeout(() => {
+      const scrollTop = window.scrollY || document.documentElement.scrollTop
+
+      if (scrollTop > lastScrollTop && window.scrollY > 100) {
+        setIsSticky(true)
+      } else if (scrollTop < lastScrollTop) {
+        setIsSticky(false)
+      }
+
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop
+    }, 10)
+  }
+
+  useEventListener('scroll', handleScroll)
+
   return (
     <S.Header as={Container} forwardedAs="header">
       <S.HeaderLogo>
         <Typography content="Logo" inheritedColorScheme="dark" />
       </S.HeaderLogo>
       <Visible breakpoints={['lg', 'xl', 'xxl']}>
-        <S.HeaderMenu>
-          <Button label="we are" variant="secondary" inheritedColorScheme="dark" />
-          <Button label="we do" variant="secondary" inheritedColorScheme="dark" />
-          <Button label="careers" variant="secondary" inheritedColorScheme="dark" />
-          <Button label="contact us" variant="secondary" inheritedColorScheme="dark" />
+        <S.HeaderMenu isSticky={isSticky}>
+          <Button label="we are" variant="secondary" inheritedColorScheme="dark" scrollTo="we-are" />
+          <Button label="we do" variant="secondary" inheritedColorScheme="dark" scrollTo="we-do" />
+          <Button label="careers" variant="secondary" inheritedColorScheme="dark" scrollTo="careers" />
+          <Button label="contact us" variant="secondary" inheritedColorScheme="dark" scrollTo="contact-us" />
         </S.HeaderMenu>
       </Visible>
     </S.Header>
--- src/components/layouts/Header/styles.js
+++ src/components/layouts/Header/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Header = styled.header`
   width: 100%;
@@ -9,7 +9,7 @@ export const Header = styled.header`
   top: 0;
   left: 0;
   right: 0;
-  padding: ${({ theme }) => theme.rem(40, 0, 0)};
+  padding-top: ${({ theme }) => theme.rem(40)};
   z-index: ${({ theme }) => theme.layers.header};
 `
 
@@ -19,4 +19,14 @@ export const HeaderMenu = styled.div`
   display: flex;
   align-items: center;
   gap: ${({ theme }) => theme.rem(24)};
+  padding: ${({ theme }) => theme.rem(0, 8)};
+  background-image: linear-gradient(0deg, blue, blue);
+  background-position: left 100%;
+  background-repeat: no-repeat;
+  background-size: 0% 100%;
+  transition: background ${({ theme }) => theme.timing.fast};
+
+  ${({ isSticky }) => isSticky && css`
+    background-size: 100% 100%;
+  `}
 `
